fd206a5f6887c68b98393801ec0a9fc849d02233
Emma Logsdon <emma.logsdon2@gmail.com>
2017-07-31 11:26:36 -0400

    things

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 
-def post_list(request, pk):
+def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
