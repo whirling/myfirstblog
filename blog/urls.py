70ca4ab9604c2fda716252ba0ba6e7b1c0118a36
Emma Logsdon <emma.logsdon2@gmail.com>
2017-07-31 13:49:38 -0400

    logging in

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,5 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from . import views
-from django.contrib import admin
-
-from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
@@ -12,8 +9,4 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
-    url(r'', include('blog.urls')),
 ]
