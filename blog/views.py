5e2b532616a6342f050adcfdb7b56b5010b8c9a4
Emma Logsdon <emma.logsdon2@gmail.com>
2017-07-31 11:12:06 -0400

    things

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import render, get_object_or_404
 def post_list(request, pk):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
-    Post.objects.get(pk=pk)
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
